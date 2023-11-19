409c918c0eb11366b46fb331531928ea4e620811
Kardee12 <emailclub72@gmail.com>
2023-11-19 15:47:35 -0800

    profile.html Completed. User can now make changes to profile details or can even delete their account! NEED TO DO: Complete Forms + Charts.

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
+from datetime import datetime
 
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from passlib.hash import pbkdf2_sha256 as sha256
 
 from db import get_db, init_db_command
+from helpers.loginAuth import login_required
 
 app = Flask(__name__)
 app.config['DATABASE'] = 'finance_tracker.db'
 app.cli.add_command(init_db_command)
 app.secret_key = "TEST_SECRET"
+
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
@@ -19,7 +22,7 @@ def register():
         name = request.form['name']
         nameL = name.split()
         firstname = nameL[0]
-        lastname = nameL[1]
+        lastname = nameL.pop()
         db = get_db()
         error = None
         cursor = db.cursor()
@@ -43,12 +46,12 @@ def register():
             except sqlite3.IntegrityError as e:
                 error = 'An unexpected error occurred. Please try again.'
         if error:
-            return redirect(url_for('hello_world1'))
-        return redirect('https://www.google.com')
+            flash('There was an error registering you. Try again', 'danger')
+        return redirect(url_for('dashboard'))
     return render_template('register.html')
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
@@ -73,40 +76,77 @@ def login():
             return redirect(url_for('dashboard'))
     return render_template('login.html')
 
+
 @app.route('/logout')
 def logout():
-    session.pop('id', None)
-    session.pop('loggedin', None)
+    session.clear()
     flash('You have been logged out.')
     return redirect(url_for('login'))
 
 
 @app.route('/home')
 def home():
-
     return redirect(url_for('login'))
 
+
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    if 'loggedin' in session:
-        user = {
-            'userId': session['userID'],
-            'firstName': session['firstName'],
-            'email':session['email'],
-            'lastName': session['lastName']
-        }
-        return render_template('Authenticated/dashboard.html', user = user)
-    else:
-        flash('You must be logged in to view the dashboard.', 'danger')
-        return redirect(url_for('login'))
+    user = {
+        'userId': session['userID'],
+        'firstName': session['firstName'],
+        'email': session['email'],
+        'lastName': session['lastName']
+    }
+
+    currentdatetime = datetime.now()
+    currentMonth = currentdatetime.strftime('%B')
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('SELECT SUM(BudgetAmount) AS TotalBudget FROM Budget WHERE Month = ? AND UserID = ?', (currentMonth, user['userId']))
+    budget = cursor.fetchone()
+    total_budget = budget['TotalBudget']
+    print(total_budget)
+    return render_template('Authenticated/dashboard.html', user = user)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('Authenticated/authenticated_base.html')
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    db = get_db()
+    cursor = db.cursor()
+
+    if request.method == 'POST':
+        if 'delete_profile' in request.form:
+            user_id = session['userID']
+            cursor.execute('DELETE FROM User WHERE UserID = ?', (user_id,))
+            db.commit()
+            session.clear()
+            flash('Your account has been successfully deleted.', 'success')
+            return redirect(url_for('register'))
+
+        elif 'save_changes' in request.form:
+            firstname = request.form.get('firstName')
+            lastname = request.form.get('lastName')
+            email = request.form.get('email')
+            cursor.execute('UPDATE User SET FirstName = ?, LastName = ?, Email = ? WHERE UserID = ?',
+                           (firstname, lastname, email, session['userID']))
+            db.commit()
+            session['firstName'] = firstname
+            session['lastName'] = lastname
+            session['email'] = email
+            flash('Profile successfully updated.', 'success')
+    user = {
+        'userId': session['userID'],
+        'firstName': session['firstName'],
+        'lastName': session['lastName'],
+        'email': session['email']
+    }
+    return render_template('Authenticated/profile.html', user=user)
 
 
 if __name__ == '__main__':
---
+++ helpers/loginAuth.py
@@ -0,0 +1,13 @@
+from functools import wraps
+from flask import redirect, session, url_for, flash
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'loggedin' not in session:
+            flash('You need to be logged in to access this page.', 'danger')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+
+    return decorated_function
