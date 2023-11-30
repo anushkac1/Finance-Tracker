e3d3adc8dfeacdadadca7f3f73972674c19168aa
Kardee12 <emailclub72@gmail.com>
2023-11-29 19:58:36 -0800

    Made it so that the userid is now checked as wellfor payment info. Also added Edit Expense Form stuff.
    TODO:
    -- Table for Expenses
    -- Graphs
    -- Monthly Summary

--- app.py
+++ app.py
@@ -26,21 +26,21 @@ def register():
         error = None
         cursor = db.cursor()
         cursor.execute('SELECT * FROM User WHERE Email = ?', (email,))
-        email_check = cursor.fetchone()
+        emailCheck = cursor.fetchone()
         cursor.execute('SELECT * FROM User WHERE FirstName = ? AND LastName = ? AND Email = ?',
                        (firstname, lastname, email))
         name_email_check = cursor.fetchone()
-        if email_check:
+        if emailCheck:
             error = 'Email already registered.'
             flash(error, 'danger')
         elif name_email_check:
             error = 'A user with the same first name, last name, and email already exists.'
             flash(error, 'danger')
-        hashed_password = sha256.hash(password)
+        hashedPwd = sha256.hash(password)
         if error is None:
             try:
                 cursor.execute('INSERT INTO User (Email, Password, FirstName, LastName) VALUES (?, ?, ?, ?)',
-                               (email, hashed_password, firstname, lastname))
+                               (email, hashedPwd, firstname, lastname))
                 db.commit()
                 # Set session variables after successful registration and login
                 cursor.execute('SELECT * FROM User WHERE Email = ?', (email,))
@@ -114,7 +114,7 @@ def dashboard():
     cursor.execute('SELECT SUM(BudgetAmount) AS TotalBudget FROM Budget WHERE Month = ? AND UserID = ?',
                    (currentMonth, user['userId']))
     budget = cursor.fetchone()
-    total_budget = budget['TotalBudget']
+    totalBudget = budget['TotalBudget']
     return render_template('Authenticated/dashboard.html', user = user)
 
 
@@ -128,7 +128,6 @@ def index():
 def profile():
     db = get_db()
     cursor = db.cursor()
-
     if request.method == 'POST':
         if 'delete_profile' in request.form:
             userID = session['userID']
@@ -165,7 +164,8 @@ def profile():
     }
     return render_template('Authenticated/profile.html', user = user)
 
-@app.route('/expense-form', methods = ['GET', 'POST'])
+
+@app.route('/expenseform', methods = ['GET', 'POST'])
 @login_required
 def expenseForm():
     db = get_db()
@@ -173,11 +173,9 @@ def expenseForm():
 
     cursor.execute('SELECT CategoryID, CategoryName FROM Category')
     categories = cursor.fetchall()
-
     cursor.execute('SELECT PaymentMethodID, PaymentMethodName FROM PaymentMethod WHERE UserID = ?',
                    (session['userID'],))
     paymentMethods = cursor.fetchall()
-
     if request.method == 'POST':
         print(request.form)
         item = request.form['item']
@@ -194,7 +192,6 @@ def expenseForm():
             categoryId = cursor.lastrowid
         elif selectedCategory:
             categoryId = int(selectedCategory)
-
         paymentMethodId = None
         if selectedPaymentMethod == 'new-method' and newPaymentMethodName:
             cursor.execute('INSERT INTO PaymentMethod (PaymentMethodName, UserID) VALUES (?, ?)',
@@ -203,19 +200,15 @@ def expenseForm():
             paymentMethodId = cursor.lastrowid
         elif selectedPaymentMethod:
             paymentMethodId = int(selectedPaymentMethod)
-
         if categoryId is not None and paymentMethodId is not None:
             userId = session['userID']
             cursor.execute('INSERT INTO ExpenseItem (UserID, Item, Amount, Date, CategoryID) VALUES (?, ?, ?, ?, ?)',
                            (userId, item, amount, date, categoryId))
             db.commit()
             expenseId = cursor.lastrowid
-
-            # Linking ExpenseItem with PaymentMethod
             cursor.execute('INSERT INTO ExpensePaymentMethod (ExpenseID, PaymentMethodID) VALUES (?, ?)',
                            (expenseId, paymentMethodId))
             db.commit()
-
             flash('Expense added successfully.', 'success')
         else:
             flash('Please select valid category and payment method.', 'danger')
@@ -224,44 +217,104 @@ def expenseForm():
 
     return render_template('Authenticated/expenseform.html', categories = categories, payment_methods = paymentMethods)
 
-@app.route('/manage-payments', methods = ['GET', 'POST'])
+
+@app.route('/editexpense/<int:expenseID>', methods = ['GET', 'POST'])
+@login_required
+def editExpense(expenseID):
+    db = get_db()
+    cursor = db.cursor()
+    if request.method == 'GET':
+        cursor.execute('SELECT * FROM ExpenseItem WHERE UserID = ?', (session['userID'],))
+        expense = cursor.fetchone()
+        if expense is None:
+            return redirect(url_for('expenseForm'))
+        cursor.execute('SELECT CategoryID, CategoryName FROM Category')
+        categories = cursor.fetchall()
+        cursor.execute('SELECT PaymentMethodID, PaymentMethodName FROM PaymentMethod WHERE UserID = ?',
+                       (session['userID'],))
+        paymentMethods = cursor.fetchall()
+        return render_template('Authenticated/editexpenses.html', expense = expense, categories = categories,
+                               payment_methods = paymentMethods)
+    if request.method == 'POST':
+        item = request.form.get('item')
+        amount = request.form.get('amount')
+        date = request.form.get('date')
+        categoryId = request.form.get('category')
+        newCategory = request.form.get('new-category')
+        paymentMethodId = request.form.get('payment-method-select')
+        newPaymentMethod = request.form.get('new-payment-method')
+        if newCategory:
+            cursor.execute('INSERT INTO Category (CategoryName) VALUES (?)', (newCategory,))
+            db.commit()
+            categoryId = cursor.lastrowid
+        if newPaymentMethod:
+            cursor.execute('INSERT INTO PaymentMethod (PaymentMethodName, UserID) VALUES (?, ?)',
+                           (newPaymentMethod, session['userID']))
+            db.commit()
+            paymentMethodId = cursor.lastrowid
+        cursor.execute('''
+            UPDATE ExpenseItem
+            SET Item = ?, Amount = ?, Date = ?, CategoryID = ?
+            WHERE ExpenseID = ? AND UserID = ?
+        ''', (item, amount, date, categoryId, expenseID, session['userID']))
+        db.commit()
+        cursor.execute('''
+            UPDATE ExpensePaymentMethod
+            SET PaymentMethodID = ?
+            WHERE ExpenseID = ?
+        ''', (paymentMethodId, expenseID))
+        db.commit()
+        return redirect(url_for('dashboard'))
+
+
+@app.route('/managepayments', methods = ['GET', 'POST'])
 @login_required
 def managePayments():
     db = get_db()
     cursor = db.cursor()
-    paymentMethodInUse = False
     id = session['userID']
+    paymentMethodInUse = False
     if 'delete' in request.form:
         paymentMethodIdToDelete = request.form['paymentMethodId']
-        replacementMethod = request.form.get('replacementMethod', None)
-        newPaymentMethodName = request.form.get('newPaymentMethodName', None)
-        cursor.execute('SELECT * FROM ExpensePaymentMethod WHERE PaymentMethodID = ?', (paymentMethodIdToDelete,))
-        if cursor.fetchone():
+        cursor.execute(
+            '''SELECT ExpenseID, Item, Amount, Date FROM ExpenseItem 
+               WHERE ExpenseID IN (SELECT ExpenseID FROM ExpensePaymentMethod WHERE PaymentMethodID = ?)
+               AND UserID = ?''',
+            (paymentMethodIdToDelete, id))
+        expenseItems = cursor.fetchall()
+        if expenseItems:
             paymentMethodInUse = True
+            replacementMethod = request.form.get('replacementMethod', None)
+            newPaymentMethodName = request.form.get('newPaymentMethodName', None)
             if replacementMethod == 'new' and newPaymentMethodName:
                 cursor.execute('INSERT INTO PaymentMethod (PaymentMethodName, UserID) VALUES (?, ?)',
                                (newPaymentMethodName, id))
                 db.commit()
                 replacementMethod = cursor.lastrowid
-            cursor.execute('UPDATE ExpensePaymentMethod SET PaymentMethodID = ? WHERE PaymentMethodID = ?',
-                           (replacementMethod, paymentMethodIdToDelete))
+            if replacementMethod and replacementMethod != 'new':
+                cursor.execute(
+                    'UPDATE ExpensePaymentMethod SET PaymentMethodID = ? WHERE PaymentMethodID = ? AND ExpenseID IN ('
+                    'SELECT ExpenseID FROM ExpenseItem WHERE UserID = ?)',
+                    (replacementMethod, paymentMethodIdToDelete, id))
+                db.commit()
+        if not paymentMethodInUse:
+            cursor.execute('DELETE FROM PaymentMethod WHERE PaymentMethodID = ? AND UserID = ?',
+                           (paymentMethodIdToDelete, id))
             db.commit()
-        cursor.execute('DELETE FROM PaymentMethod WHERE PaymentMethodID = ?', (paymentMethodIdToDelete,))
-        db.commit()
-        flash('Payment method deleted and replaced successfully.', 'success')
+            flash('Payment method deleted successfully.', 'success')
     elif "add" in request.form:
         paymentMethodName = request.form['paymentMethodName']
-        print(paymentMethodName)
-        cursor.execute('SELECT * FROM PaymentMethod WHERE PaymentMethodName = ?', (paymentMethodName,))
+        cursor.execute('SELECT * FROM PaymentMethod WHERE PaymentMethodName = ? AND UserID = ?',
+                       (paymentMethodName, id))
         if cursor.fetchone():
-            flash("Payment has already been added", 'danger')
+            flash("Payment method already exists", 'danger')
         else:
-            cursor.execute('INSERT INTO PaymentMethod (UserID, PaymentMethodName) VALUES (?,?)',
+            cursor.execute('INSERT INTO PaymentMethod (UserID, PaymentMethodName) VALUES (?, ?)',
                            (id, paymentMethodName))
             db.commit()
-    cursor.execute('SELECT * FROM PaymentMethod WHERE UserID = ?', (session['userID'],))
+            flash('Payment method added successfully.', 'success')
+    cursor.execute('SELECT * FROM PaymentMethod WHERE UserID = ?', (id,))
     paymentMethods = cursor.fetchall()
-
     return render_template('Authenticated/managepayments.html', paymentMethods = paymentMethods,
                            paymentMethodInUse = paymentMethodInUse)
 
